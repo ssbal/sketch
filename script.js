0025ce76fcfd135ab93ab04e39aa55c18afec07f
Sukhdeep <sukhdeep29723@gmail.com>
2023-08-25 14:41:03 +0530

    Create a following trail
    Select all grid elements in such a way that whenever one hovers over the grid it makes a mouse pointer following trail on the grid

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ const grid = document.querySelector('.grid');
 for (let i = 1; i <= 16; i++) {
   const row = document.createElement('div');
   row.setAttribute('class', 'row');
+  row.style.height = '20px';
 
-  /**
-   * Alternative via CSS
-   * 1. Create a row for every sixteen divs through for loops
-   * 2. Now add flexbox to that particluar row for every particular row of grid
-   */
   for (let j = 1; j <= 16; j++) {
     const box = document.createElement('span');
     box.setAttribute('class', 'box');
+    box.style.width = '20px';
+    box.style.height = '20px';
+
     row.appendChild(box);
-    box.textContent = i;
   }
 
   grid.appendChild(row);
 }
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+  box.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'orangered';
+  });
+});
