792b58a668a86af793e5a8aa65a2cc16aa82dbbf
Sukhdeep <sukhdeep29723@gmail.com>
2023-08-25 14:59:56 +0530

    Create fixed size grid and function createGrid
    To get n*n grid on demand

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const grid = document.querySelector('.grid');
 
-for (let i = 1; i <= 16; i++) {
-  const row = document.createElement('div');
-  row.setAttribute('class', 'row');
-  row.style.height = '20px';
+function createGrid(size) {
+  for (let i = 1; i <= size; i++) {
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    row.style.height = `${Math.floor(500 / size)}px`;
 
-  for (let j = 1; j <= 16; j++) {
-    const box = document.createElement('span');
-    box.setAttribute('class', 'box');
-    box.style.width = '20px';
-    box.style.height = '20px';
+    for (let j = 1; j <= size; j++) {
+      const box = document.createElement('span');
+      box.setAttribute('class', 'box');
+      box.style.width = `${Math.floor(500 / size) - 0.1}px`;
+      box.style.height = `${Math.floor(500 / size)}px`;
 
-    row.appendChild(box);
+      row.appendChild(box);
+    }
+
+    grid.appendChild(row);
   }
-
-  grid.appendChild(row);
 }
+
+createGrid(20);
 
 const boxes = document.querySelectorAll('.box');
 
